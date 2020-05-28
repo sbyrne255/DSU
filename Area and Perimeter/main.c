2193a6d36a76ada9e60a4427ba9542a2e6469c99
Steven Byrne <Stevenbyrne255@gmail.com>
2020-05-28 18:59:08 -0500

    Morning Update
    Updated proper float/int usage.
    Fixed minor spelling and logic errors.
    Added Designed Docs.
    Added Comments.
    Flushed output with Linux/multi variant method instead of flush(stdin) which failed on some linux distros.

--- Area and Perimeter/main.c
+++ Area and Perimeter/main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*H**********************************************************************
+* FILENAME :        main.c             DESIGN REF: D-Doc / Code Shape Calc
+*
+* DESCRIPTION :
+*       DSU PROGRAMMING ASSIGNMENT 1, WEEK 2, PROGRAM IS REQUIRED TO TAKE AN INT INPUT
+*       AND DISPLAY THE AREA AND CIRCUMFERENCE OF A CIRCLE IF THE INPUT WAS THE RADIUS
+*       AND DISPLAY THE AREA AND PERIMETER IF THE INPUT WAS ONE SIDE OF A SQUARE.
+*
+* NOTES :
+*       This file is part of a DSU assignment completed on 05/28/2020
+*       GNU Licensing applied, see more where you got this file or at https://github.com/sbyrne255
+*       There are known security vulnerabilities with this code, note-ably the use of scanf without buffer overflow protections
+*
+* AUTHOR :    Steven Byrne        START DATE :    27 May 20
+*
+**********************************************************************H*/
 
 int main()
 {
-    //Used to track quantity of current item being listed.
+    //Used as base input, expected to be an int input with float or int output.
     int radius;
     //Used for exit condition.
     int exit = 0;
@@ -13,42 +29,25 @@ int main()
     //Intro to program
     printf("#############################################\n");
     printf("#############################################\n");
-    printf("##### Welcome to Area & Perimeter 3000  #####\n");
+    printf("######## Welcome to Shape Calc 3000  ########\n");
     printf("#############################################\n");
     do{
         printf("Please enter the radius of a circle as a whole number: ");
-        if((scanf("%d", &radius) == 1) && (radius > 0)){
-
-
-printf("%f\n",radius);fflush(stdin);
-            float c_area = (pi * (radius * radius));
-printf("%f\n",radius);fflush(stdin);
-            float c_circumference = (2 * (pi * radius));
-printf("%f\n",radius);fflush(stdin);
-            float s_area = (radius * radius);
-printf("%f\n",radius);fflush(stdin);
-            float s_perimiter = radius * 4;
-printf("%f\n",radius);fflush(stdin);
+        if((scanf("%d", &radius) == 1) && (radius > 0)){//Input is an int that is greater than 0.
             printf("Circle Information:\n");
-            printf("    Area of the circle is: %f\n", c_area);
-printf("%f\n",radius);fflush(stdin);
-            printf("    Circumference of the circle is: %f\n", c_circumference);
-printf("%f\n",radius);fflush(stdin);
-            printf("Radius is equal to: %f\n", radius);
-printf("%f\n",radius);fflush(stdin);
-            /*
-
+            printf("    Area of the circle is: %f\n", (float)(pi * (radius * radius)));//Could use POW with a header include, but this works for what we're doing.
+            printf("    Circumference of the circle is: %f\n", (float)(2 * (pi * radius)));
             printf("Square Information:\n");
-            printf("    Area of the square is: %f\n", s_area);
-            printf("    Perimeter of the square is: %f\n", s_perimiter);
-*/
+            printf("    Area of the square is: %d\n", radius * radius);
+            printf("    Perimeter of the square is: %d\n", radius * 4);
+
             printf("Would you like to calculate another area and perimeter (0) or exit (1-9)? \n");
             scanf("%d", &exit);
 
         } else {
             printf("Please enter a valid radius...\n   radius should be a whole number (no fractions). \n   Radius must be greater than 0.\n");
-            //Clear invalid character from buffer
-            fflush(stdin);
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF);
             continue;
         }
     }
--- Cashier/main.c
+++ Cashier/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*H**********************************************************************
+* FILENAME :        main.c             DESIGN REF: D-Doc / Cashier
+*
+* DESCRIPTION :
+*       DSU PROGRAMMING ASSIGNMENT 1, WEEK 2, PROGRAM IS REQUIRED TO TAKE AN PRICE AS AN INPUT (FLOAT)
+*       AND QUANTITY PURCHASED (INT) AND CALCUATE/DISPLAY THE SUB-TOTAL, TAX, AND TOTAL.
+*
+* NOTES :
+*       This file is part of a DSU assignment completed on 05/28/2020
+*       GNU Licensing applied, see more where you got this file or at https://github.com/sbyrne255
+*       There are known security vulnerabilities with this code, note-ably the use of scanf without buffer overflow protections
+*       There is no protection against input related attacks. The input of INF will result in the output being INF.
+*
+* AUTHOR :    Steven Byrne        START DATE :    27 May 20
+*
+**********************************************************************H*/
 int main()
 {
     //Used to track quantity of current item being listed.
@@ -23,25 +38,27 @@ int main()
     do{
         printf("Please enter the price of the item: ");
         if((scanf("%f", &unit_price) == 1) && (unit_price > 0)){
-                printf("Enter the quantity you are purchasing: ");
-                if(scanf("%d", &qty) == 1){
-                    price = qty * unit_price;
-                    printf("    Item Cost (sub-total): %.2f\n", price);
-                    printf("    Tax Collected (Tax): %.2f\n", (tax_rate * price) );
-                    printf("    Total (Tax and Items): %.2f\n", price + (price*tax_rate));
+            printf("Enter the quantity you are purchasing: ");
+            if(scanf("%d", &qty) == 1 && (qty > 0)){
+                price = qty * unit_price;
+                printf("    Item Cost (sub-total): %.2f\n", price);
+                printf("    Tax Collected (Tax): %.2f\n", (tax_rate * price) );
+                printf("    Total (Tax and Items): %.2f\n", price + (price*tax_rate));
 
-                    printf("Would you like to calculate another item (0) or exit (1-9)? \n");
-                    scanf("%d", &exit);
-                }
+                printf("Would you like to calculate another item (0) or exit (1-9)? \n");
+                scanf("%d", &exit);
+            } else {
+                printf("Please enter a valid price...\n   Prices should be formatted as: 1.29 without special characters (except the decimal). \n   Prices must be greater than 0.\n");
+                int ch;
+                while ((ch = getchar()) != '\n' && ch != EOF);
+                continue;
+            }
         } else {
             printf("Please enter a valid price...\n   Prices should be formatted as: 1.29 without special characters (except the decimal). \n   Prices must be greater than 0.\n");
-            //Clear invalid character from buffer
-            fflush(stdin);
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF);
             continue;
         }
-
-
-
     }
     while(exit == 0);
 }
